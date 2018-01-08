83e33926967e39b644c816f273333fb8bd5aded1
Marnix van de Sande <m.a.j.vandesande@students.uu.nl>
2018-01-08 14:43:59 +0100

    Create .py file

---
+++ precipitation_calculations_MvdS.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 12:38:03 2018
+
+@author: Marnix van de Sande
+"""
+
+import json
+
+with open('precipitation.json') as file:
+    precipitation_data = json.load(file)
+
+seattle_code = "GHCND:US1WAKG0038"
+seattle_data = []
+for item in precipitation_data:
+    if precipitation_data[item]['station'] == seattle_code:
+        seattle_data.append(precipitation_data[item])
