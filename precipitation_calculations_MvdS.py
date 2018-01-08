765dd86525aa9e7a2cc77ed904b39ecfea3179fe
Marnix van de Sande <m.a.j.vandesande@students.uu.nl>
2018-01-08 19:13:40 +0100

    Finish Part 3-Simplified

--- precipitation_calculations_MvdS.py
+++ precipitation_calculations_MvdS.py
@@ -13,17 +13,16 @@ with open('stations.csv') as file:
         (Location, State, Station) = line.strip().split(',')
         stations[Location] = {'State' : State, 'Station' : Station}
 
-#Calculate the yearly precipitation in the entire country --> For % state. I now used a loop, but is there a way to sum() this?
-total_country_precipitation = 0
-for item in range(len(precipitation_data)):
-    total_country_precipitation += precipitation_data[item]['value']
-    
 result_dictionary = {} #Create a dict for final results
+#Calculate the yearly precipitation in the entire country --> Values are added in the most nested loop, to make sure everything is included.
+total_country_precipitation = 0
+
+#Create a loop to go over all the weather stations
 for chosen_station in stations.keys():
     #Define weather station code of interest and create an empty list for the filtered data
-    state = stations[chosen_station]['State']
-    station_code = stations[chosen_station]['Station']
-    station_data = []
+    state = stations[chosen_station]['State'] #State code
+    station_code = stations[chosen_station]['Station'] #Station code
+    station_data = [] #Empty list to add station data
     
     #Filter out all the entries for the station of interest, and add them to the newly created list
     for item in range(len(precipitation_data)):
@@ -31,15 +30,16 @@ for chosen_station in stations.keys():
             station_data.append(precipitation_data[item])
     
     #Organise data per month
-    precipitation_per_month = [] #New final data list [Jan, Feb, Mar...]
+    precipitation_per_month = [] #New data list for all the months [Jan, Feb, Mar...]
     for month in range(1, 13): #Loop over the 12 months, sum up all values for that month
-        tmp_data = []
+        month_data = 0
         for measurement in range(len(station_data)): # Check for all entries whether they correspond to the 'current' month
             if '2010-'+str(month).zfill(2) in station_data[measurement]['date']:
-                tmp_data.append(station_data[measurement]['value']) #Add all the values for this month to a temporary list
-        precipitation_per_month.append(sum(tmp_data)) #Add the sum of this temporary list (all the measurements for one month) to the final data.
+                month_data += station_data[measurement]['value'] #Sum up all the values for this month
+                total_country_precipitation += station_data[measurement]['value'] #Also add the value to the total country rain count
+        precipitation_per_month.append(month_data) #Add the sum for this month to the month list.
     
-    #Calculate sum of prep for the whole year
+    #Calculate sum of precipitation for the whole year for this station
     precipitation_whole_year = sum(precipitation_per_month)
     #Calculate relative precipitation (% compared to whole year)
     relative_monthly_precipitation =[]
